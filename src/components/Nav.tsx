d69083adeb518fd0b9dbc52fbcabed451f4c5e95
Rob Harding <robbie.harding@hotmail.com>
2023-06-08 17:40:18 -0400

    Better looking nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,8 @@
 export default function Nav() {
   return (
-    <div>
-      <h1>Nav</h1>
+    <div className="p-2 w-screen flex flex-col gap-1">
+      <h1 className="text-5xl font-bold tracking-[-0.035em]">HABITS</h1>
+      <hr className="border-b border-black" />
     </div>
   );
 }
