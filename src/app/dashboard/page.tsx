9167c3612acb5bb81d349ebda583fd258edaa1a4
Rob Harding <robbie.harding@hotmail.com>
2023-06-08 17:13:39 -0400

    Add and test shadcn ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import { Toggle } from "@/components/ui/toggle";
+import { Bold } from "lucide-react";
+
 export default function Home() {
   return (
     <div className="mt-10">
       <h1>Dasboard</h1>
+      <Toggle variant="outline" aria-label="Toggle italic">
+        <Bold className="h-4 w-4" />
+      </Toggle>
     </div>
   );
 }
